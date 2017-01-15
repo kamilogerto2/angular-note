2a18633c1143c2b5ff3ead7af0a087793df304af
Kamil Mikosz <mikosz_kamil@bitbucket.org>
2017-01-15 15:59:36 +0100

    few tests for data manager

--- src/tests/dataManager.spec.js
+++ src/tests/dataManager.spec.js
@@ -1,3 +1,44 @@
 /**
  * Created by Kamil on 2017-01-15.
  */
+describe('Data manager', function () {
+	beforeEach(module('app', function (dataManger) {
+		this.dataManager = new dataManger;
+	}));
+
+	it('should initialize data provider', function () {
+		except().toBe(false);
+	});
+
+	it('should pass data to provider', function () {
+		except().toBe(false);
+	});
+
+	it('should return value from provider', function () {
+		except().toBe(false);
+	});
+
+	it('should return list of notes from provider', function () {
+		except().toBe(false);
+	});
+
+	it('should edit data in provider', function () {
+		except().toBe(false);
+	});
+
+	it('should clean notes data in provider', function () {
+		except().toBe(false);
+	});
+
+	it('should return an error message when error will occured', function () {
+		except().toBe(false);
+	});
+
+	it('should check if any provider is available an initialized', function () {
+		except().toBe(false);
+	});
+
+	it('should provider manually', function () {
+		except().toBe(false);
+	});
+});
