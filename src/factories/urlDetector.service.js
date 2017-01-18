9a883f882f415c80b4a58bcd16afd332d94dbb52
Kamil Mikosz <mikosz_kamil@bitbucket.org>
2017-01-18 23:15:23 +0100

    basic version of url detector with tests

--- src/factories/urlDetector.service.js
+++ src/factories/urlDetector.service.js
@@ -2,4 +2,9 @@
  * Created by Kamil on 2017-01-18.
  */
 angular.module('app').service('urlDetector', function () {
+	this.detect = function (text) {
+		var urlExpression = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+		var regex = new RegExp(urlExpression);
+		return text.match(regex);
+	}
 });
--- src/tests/urlDetector.spec.js
+++ src/tests/urlDetector.spec.js
@@ -1,3 +1,35 @@
 /**
  * Created by Kamil on 2017-01-18.
  */
+describe('Url detector', function () {
+	//given
+	beforeEach(module('app'));
+
+	it('should detect url in url address', angular.mock.inject(function (urlDetector) {
+		expect(urlDetector.detect('www.magic.com/maybe/not').length).toBe(1);
+		expect(urlDetector.detect('http://www.magic.com/maybe/not').length).toBe(1);
+		expect(urlDetector.detect('https://www.magic.com/maybe/not').length).toBe(1);
+		expect(urlDetector.detect('magic.com/maybe/not').length).toBe(1);
+		expect(urlDetector.detect('magic.com').length).toBe(1);
+	}));
+
+	it('should not detect url in url phrases', angular.mock.inject(function (urlDetector) {
+		expect(urlDetector.detect('www.magic')).toBe(null);
+		expect(urlDetector.detect('w://agro')).toBe(null);
+		expect(urlDetector.detect('agro.magre')).toBe(null);
+		expect(urlDetector.detect('agro/magre/padre')).toBe(null);
+	}));
+
+	it('should detect url in simple text', angular.mock.inject(function (urlDetector) {
+
+	}));
+
+	it('should detect url in more complex text', angular.mock.inject(function (urlDetector) {
+
+	}));
+
+	it('should detect more url address in complex text', angular.mock.inject(function (urlDetector) {
+
+	}));
+
+});
