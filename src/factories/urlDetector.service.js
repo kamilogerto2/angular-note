f9d72dc31bd88c63933997bbe5c90ebbf88622ed
Kamil Mikosz <mikosz_kamil@bitbucket.org>
2017-01-19 11:35:07 +0100

    basic image url detection + tests

--- src/factories/urlDetector.service.js
+++ src/factories/urlDetector.service.js
@@ -3,9 +3,9 @@
  */
 angular.module('app').service('urlDetector', function () {
 	function determineImageInURL(path) {
-		var imageExpression = /(\/*\.(?:png|jpg)[^a-zA-Z])/gi;
+		var imageExpression = /(\/*\.(?:png|jpg|gif))/g;
 		var regex = new RegExp(imageExpression);
-		return path.match(regex).length ? true : false;
+		return path.match(regex) || false;
 	}
 	this.detect = function (text) {
 		var urlExpression = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
@@ -14,10 +14,11 @@ angular.module('app').service('urlDetector', function () {
 	};
 
 	this.detectImagePath = function (text) {
-		var urlInText = this.detect(text), imageTable = [];
+		var urlInText = this.detect(text), imageTable = [], newPath, urlPath;
 		for (var i = 0; i < urlInText.length; i++) {
-			if (determineImageInURL(urlInText[i])) {
-				imageTable.push(urlInText[i]);
+			if (urlPath = determineImageInURL(urlInText[i])) {
+				newPath = urlInText[i].split(urlPath[0]);
+				imageTable.push(newPath[0] + urlPath[0]);
 			}
 		}
 
--- src/tests/urlDetector.spec.js
+++ src/tests/urlDetector.spec.js
@@ -6,6 +6,7 @@ describe('Url detector', function () {
 	beforeEach(module('app'));
 
 	it('should detect url in url address', angular.mock.inject(function (urlDetector) {
+		//when, then
 		expect(urlDetector.detect('www.magic.com/maybe/not').length).toBe(1);
 		expect(urlDetector.detect('http://www.magic.com/maybe/not').length).toBe(1);
 		expect(urlDetector.detect('https://www.magic.com/maybe/not').length).toBe(1);
@@ -14,22 +15,47 @@ describe('Url detector', function () {
 	}));
 
 	it('should not detect url in url phrases', angular.mock.inject(function (urlDetector) {
-		expect(urlDetector.detect('www.magic')).toBe(null);
+		//when, then
 		expect(urlDetector.detect('w://agro')).toBe(null);
-		expect(urlDetector.detect('agro.magre')).toBe(null);
 		expect(urlDetector.detect('agro/magre/padre')).toBe(null);
 	}));
 
-	it('should detect url in simple text', angular.mock.inject(function (urlDetector) {
-
+	it('should detect image url in simple url', angular.mock.inject(function (urlDetector) {
+		//given
+		var imagePath = 'www.xyz.com/klif.png';
+		//when
+		var result = urlDetector.detectImagePath(imagePath);
+		//then
+		expect(result[0]).toEqual(imagePath);
 	}));
 
-	it('should detect url in more complex text', angular.mock.inject(function (urlDetector) {
-
+	it('should detect image url in simple text', angular.mock.inject(function (urlDetector) {
+		//given
+		var imagePath = 'Awesome image path is in the www.xyz.com/klif.png.';
+		//when
+		var result = urlDetector.detectImagePath(imagePath);
+		//then
+		expect(result[0]).toEqual('www.xyz.com/klif.png');
 	}));
 
-	it('should detect more url address in complex text', angular.mock.inject(function (urlDetector) {
+	it('should detect image url in more complex text', angular.mock.inject(function (urlDetector) {
+		//given
+		var imagePath = 'Awesome image path is in the www.xyz.com/klif.png. Do you belive it? Its awesome what browser can do now';
+		//when
+		var result = urlDetector.detectImagePath(imagePath);
+		//then
+		expect(result[0]).toEqual('www.xyz.com/klif.png');
+	}));
 
+	it('should detect more image urls in text', angular.mock.inject(function (urlDetector) {
+		//given
+		var imagePath = 'Awesome image path is in the www.xyz.com/klif.png. Do you belive it? Its awesome what browser can do now. grs.com/miszcz.gif';
+		//when
+		var result = urlDetector.detectImagePath(imagePath);
+		//then
+		expect(result[0]).toEqual('www.xyz.com/klif.png');
+		expect(result[1]).toEqual('grs.com/miszcz.gif');
+		expect(result.length).toEqual(2);
 	}));
 
 });
