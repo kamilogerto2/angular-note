801f8958785111b674cfa5989185601ebac35000
Kamil Mikosz <mikosz_kamil@bitbucket.org>
2017-01-20 18:54:31 +0100

    introduce note and noteList

--- src/components/note/note.js
+++ src/components/note/note.js
@@ -1,3 +1,12 @@
 /**
  * Created by Kamil on 2017-01-15.
  */
+angular
+	.module('app')
+	.component('note', {
+		templateUrl: 'components/note/note.html',
+		bindings: {
+			content: '@',
+			images: '='
+		}
+	});
--- src/components/noteList/noteList.js
+++ src/components/noteList/noteList.js
@@ -1,3 +1,13 @@
 /**
  * Created by Kamil on 2017-01-15.
  */
+angular
+	.module('app')
+	.component('note', {
+		templateUrl: 'components/note/note.html',
+		controller: NoteListController
+	});
+
+function NoteListController() {
+	
+}
